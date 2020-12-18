df0f2d3128621b71400a97be3ee9174df1838abd
Luno-front-end <63469409+Luno-front-end@users.noreply.github.com>
2020-12-18 17:40:24 +0200

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { createPortal } from "react-dom";
 
 import "../styles/styles.css";
 
-export default function Modal({ selImage }) {
-  const modalRoot = document.querySelector("#modal-root");
-  return createPortal(
-    <div className="Overlay">
-      <div className="Modal">
-        <img src={selImage} alt="" />
-      </div>
-    </div>,
-    modalRoot
-  );
+export default class Modal extends Component {
+  // openModal = () => {
+  //   console.log();
+  // };
+  componentDidMount() {
+    window.addEventListener("keydown", this.exitModal);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.exitModal);
+  }
+  exitModal = (e) => {
+    if (e.code === "Escape") {
+      this.props.onClose();
+    }
+    console.log(e.code);
+  };
+  render() {
+    const modalRoot = document.querySelector("#modal-root");
+
+    return createPortal(
+      <div className="Overlay">
+        <div className="Modal">{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
 }
