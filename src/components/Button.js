94ad4dfc404f5ada6598587a0a10b50c7689b33e
Luno-front-end <63469409+Luno-front-end@users.noreply.github.com>
2020-12-19 02:21:24 +0200

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,13 @@ import React from "react";
 import "../styles/styles.css";
 
 function Button({ children, onClick }) {
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <div className="containerBtn-loadMore">
+      <button className="Button" onClick={onClick}>
+        {children}
+      </button>
+    </div>
+  );
   // window.scrollTo({
   //   top: document.documentElement.scrollHeight,
   //   behavior: "smooth",
